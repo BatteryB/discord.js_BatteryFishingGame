504f6c3c4d1202ca611860be319aa3029a56794b
BatteryB <130419015+BatteryB@users.noreply.github.com>
2024-04-01 15:14:19 +0900

    1.1v

--- index.mjs
+++ index.mjs
@@ -5,6 +5,7 @@ const db = new sqlite3.Database('Fish.db');
 
 const TOKEN = "TOKEN";
 
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -93,32 +94,51 @@ const fishingRodUpgrade = [
         레벨: 1,
         가격: 100000,
         실: 20,
-        철조각: 10
+        철조각: 10,
+        소요시간: 40000
     },
     {
         레벨: 2,
         가격: 250000,
         실: 40,
-        철조각: 20
+        철조각: 20,
+        소요시간: 35000
     },
     {
         레벨: 3,
         가격: 500000,
         실: 60,
-        철조각: 30
+        철조각: 30,
+        소요시간: 30000
     },
     {
         레벨: 4,
         가격: 750000,
         실: 80,
-        철조각: 40
+        철조각: 40,
+        소요시간: 25000
     },
     {
         레벨: 5,
         가격: 1000000,
         실: 100,
-        철조각: 50
+        철조각: 50,
+        소요시간: 20000
     },
+    {
+        레벨: 6,
+        가격: 1250000,
+        실: 120,
+        철조각: 60,
+        소요시간: 15000
+    },
+    {
+        레벨: 7, // 현재 7레벨이 최고레벨
+        가격: 1500000,
+        실: 140,
+        철조각: 70,
+        소요시간: 10000
+    }
 ]
 
 client.on('interactionCreate', async interaction => {
@@ -134,7 +154,7 @@ client.on('interactionCreate', async interaction => {
             await db.run("INSERT INTO user(id) VALUES(?);", [interaction.user.id]);
             await db.run("INSERT INTO item(id) VALUES(?)", [interaction.user.id]);
             await db.run("INSERT INTO fish(id) VALUES(?)", [interaction.user.id]);
-            await interaction.reply("가입되었습니다.");
+            await interaction.reply({ content: "가입되었습니다.", ephemeral: true });
         } else {
             await interaction.reply({ content: '이미 가입하셨습니다.', ephemeral: true });
         }
@@ -186,6 +206,7 @@ client.on('interactionCreate', async interaction => {
             let invenEmbed = new EmbedBuilder()
                 .setTitle(interaction.user.globalName + '님의 인벤토리')
                 .setDescription(invenTxt)
+                .setThumbnail(interaction.user.displayAvatarURL())
                 .setTimestamp();
             await interaction.reply({ embeds: [invenEmbed] });
         } else {
@@ -202,7 +223,8 @@ client.on('interactionCreate', async interaction => {
                 if (work === '낚시') {       // ==================================낚시==================================
                     db.run('UPDATE user SET work = 1 WHERE id = ?', [interaction.user.id]);
                     let pickFish = String(fishArr[Math.floor(Math.random() * fishArr.length)].물고기);
-                    let fishingTime = Math.floor(Math.random() * 5000) + 5000; // 5~10초
+                    let fishingTimeObj = fishingRodUpgrade.find(rodTime => rodTime.레벨 == userInfo.fishingRod);
+                    let fishingTime = fishingTimeObj.소요시간;
                     await interaction.reply(interaction.user.globalName + '이(가) 낚싯대를 던졌다.\n물고기가 잡히길 기다리자...');
                     setTimeout(() => {
                         db.run('UPDATE user SET work = 0 WHERE id = ?', [interaction.user.id]);
@@ -374,7 +396,8 @@ client.on('interactionCreate', async interaction => {
             let userInfo = await getUserInfo(interaction.user.id);
             let userItem = await getAllItem(interaction.user.id);
             let upgradeMaterial = fishingRodUpgrade.find(rod => rod.레벨 == userInfo.fishingRod);
-            if (upgradeMaterial != undefined) {
+            let maxLevel = fishingRodUpgrade[fishingRodUpgrade.length - 1].레벨;
+            if (upgradeMaterial.레벨 != maxLevel) {
                 if (userInfo.money < upgradeMaterial.가격) {
                     await interaction.reply({ content: '돈이 부족합니다.', ephemeral: true });
                 } else if (userItem.실 < upgradeMaterial.실) {
@@ -384,7 +407,7 @@ client.on('interactionCreate', async interaction => {
                 } else {
                     db.run(`UPDATE user SET money = money - ${upgradeMaterial.가격}, fishingRod = fishingRod + 1 WHERE id = ?`, [interaction.user.id]);
                     db.run(`UPDATE item SET 실 = 실 - ${upgradeMaterial.실}, 철조각 = 철조각 - ${upgradeMaterial.철조각}`);
-                    await interaction.reply(`${interaction.user.globalName}님이 낚싯대를 업그레이드 했습니다!\n\n*실 -${NumberConversion(upgradeMaterial.실)}개\n철조각 -${NumberConversion(upgradeMaterial.철조각)}개\n돈 -${NumberConversion(upgradeMaterial.가격)}\n낚싯대 레벨  ${userInfo.fishingRod} => ${userInfo.fishingRod + 1}*`);
+                    await interaction.reply(`${interaction.user.globalName}님이 낚싯대를 업그레이드 했습니다!\n**낚싯대 레벨  ${userInfo.fishingRod} => ${userInfo.fishingRod + 1}\n낚시 소요시간 -5초**\n\n*실 -${NumberConversion(upgradeMaterial.실)}개\n철조각 -${NumberConversion(upgradeMaterial.철조각)}개\n돈 -${NumberConversion(upgradeMaterial.가격)}*`);
                 }
             } else {
                 await interaction.reply({ content: interaction.user.globalName + '님은 현재 "최고레벨" 입니다.', ephemeral: true });
@@ -395,7 +418,7 @@ client.on('interactionCreate', async interaction => {
     }
 });
 
-function NumberConversion(Num) {
+function NumberConversion(Num) { // 숫자 변환 예) 100000 => 100,000
     return Num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
